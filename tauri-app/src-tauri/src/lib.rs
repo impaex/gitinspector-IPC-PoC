c3ea360b63733e2693ef32ad613c1b1b4e795007
Max <max.den.oudsten@gmail.com>
2025-06-29 16:35:02 +0200

    feat: make lib register CLI calling Command

--- tauri-app/src-tauri/src/lib.rs
+++ tauri-app/src-tauri/src/lib.rs
@@ -1,14 +1,14 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use commands::run_gitinspector_gui;
+
+mod commands;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        // .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![run_gitinspector_gui])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
